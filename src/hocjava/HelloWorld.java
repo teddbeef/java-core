d4ce64b1b71bb0691b31838bf7864c66741151af
Tedd <91820662+teddbeef@users.noreply.github.com>
2024-04-30 23:29:04 +0700

    print format

--- src/hocjava/HelloWorld.java
+++ src/hocjava/HelloWorld.java
@@ -3,9 +3,10 @@ package hocjava;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int a = 3;
-		int b = 6;
-		int sum = a + b;
-		System.out.println("Sum: " + sum);
+		System.out.print("learning Java");
+		System.out.println("learning Java");
+		System.out.printf("have %.1f ", 3000.33); // %f: float, %d: int, %s: string
+		System.out.printf("\nhave %d ", 3000); // %f: float, %d: int, %s: string
+		System.out.printf("\nI am %s ", "learning Java"); // %f: float, %d: int, %s: string
 	}
 }
