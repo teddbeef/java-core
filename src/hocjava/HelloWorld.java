3e22f309f50d8707700d041f020c50f0d4bf1ee1
Tedd <91820662+teddbeef@users.noreply.github.com>
2024-04-30 23:37:06 +0700

    I am

--- src/hocjava/HelloWorld.java
+++ src/hocjava/HelloWorld.java
@@ -3,10 +3,10 @@ package hocjava;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.print("learning Java");
-		System.out.println("learning Java");
-		System.out.printf("have %.1f ", 3000.33); // %f: float, %d: int, %s: string
-		System.out.printf("\nhave %d ", 3000); // %f: float, %d: int, %s: string
-		System.out.printf("\nI am %s ", "learning Java"); // %f: float, %d: int, %s: string
+		int age = 18;
+		String name;
+		name = "Teddy";
+		String text = "I am learning JAVA";
+		System.out.println("My name is " + name + " " + age + " year old" + ", and " + text);
 	}
 }
