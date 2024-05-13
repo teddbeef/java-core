9f61da7eda708ec49e0622409b621188d8fff156
Tedd <91820662+teddbeef@users.noreply.github.com>
2024-05-13 20:58:18 +0700

    java operators

---
+++ src/hocjava/JavaOperators.java
@@ -0,0 +1,19 @@
+package hocjava;
+
+public class JavaOperators {
+	public static void main(String[] args) {
+		int x = 3, y = 7;
+
+		int sum = x + y; // Addition
+		int sub = y - x; // Subtraction
+		int mul = x * y; // Multiplication
+		double div = y / x; // Division
+		float mod = y % x; // Returns the division remainder Returns the division remainder
+		System.out.println("Addition = " + sum);
+		System.out.println("Subtraction = " + sub);
+		System.out.println("Multiplication = " + mul);
+		System.out.println("Division = " + div);
+		System.out.println("Returns the division remainder Returns the division remainder = " + Math.round(mod));
+	}
+
+}
