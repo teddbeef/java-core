074c8d33341a5db33ab79d74fd6a4c3acf72e529
Tedd <91820662+teddbeef@users.noreply.github.com>
2024-07-01 22:47:57 +0700

    change code

--- src/lab/Bai07.java
+++ src/lab/Bai07.java
@@ -8,11 +8,11 @@ public class Bai07 {
 		System.out.println("HOMEWORK - LAB03 - UNIT 3");
 		Scanner sc = new Scanner(System.in);
 
-		int size;
 		System.out.println("Enter the number of size of array: ");
-		size = sc.nextInt();
+		int size = sc.nextInt();
 		int[] a = new int[size];
 		System.out.println("Enter the array element: ");
+
 		// For reading the element
 		for (int i = 0; i < size; i++) {
 			a[i] = sc.nextInt();
